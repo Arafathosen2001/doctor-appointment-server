509c1bbb135e090f498aa7a87b64324428464745
Arafathosen2001 <aarafat985@gmail.com>
2026-05-19 23:13:47 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const dotenv=require('dotenv');
+const app = express();
+const cors = require('cors');
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT;
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+      await client.connect();
+        const db = client.db('doctorAppointment');
+        const collectionDoctor = db.collection('doctor');
+    
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
