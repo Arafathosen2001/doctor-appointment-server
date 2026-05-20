f1f8657724496b8a8947ff0195ca29fa5b4bf27b
Arafathosen2001 <aarafat985@gmail.com>
2026-05-21 00:32:07 +0600

    create a POST APi for insert booking data || 2026-05-21 12:32 AM

--- index.js
+++ index.js
@@ -26,16 +26,18 @@ async function run() {
       await client.connect();
         const db = client.db('doctorAppointment');
         const collectionDoctor = db.collection('doctor');
+        const collectionBokingDoctor = db.collection('appointments');
     app.get('/doctors', async(req,res) => {
         const result = await collectionDoctor.find().toArray();
         res.send(result);
     })
-    app.get('/doctors/:did', async(req, res) => {
-      const id = req.params.did
-      console.log(id)
-      const result = await collectionDoctor.findOne({ _id: new ObjectId(id) });
-      res.send(result)
+    app.post('/appointments', async (req, res) => {
+      const bokingDoctorData = req.body;
+      const result = await collectionBokingDoctor.insertOne(bokingDoctorData)
+      res.json(result)
+      
     })
+
 
 
     await client.db("admin").command({ ping: 1 });
